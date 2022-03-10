baedfb787cc54f8500f53c022d2b03c60041cf1b
philouLeF <delcroixphilippe73@gmail.com>
2022-03-10 15:11:26 +0100

    pas grand chose

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 const words = ["Javascript", "HTML", "CSS"];
+const play = document.getElementById("play");
+play.addEventListener("click", init);
 
 function init() {
-    let live = 5;
-    let randomWord = words[Math.floor(Math.random * words.length)]
+    let lives = 5;
+    let randomWord = words[Math.floor(Math.random() * words.length)];
+    document.getElementById("randomWord").innerHTML = randomWord
 }
 
 function guess(element) {
-    for (let i = 0; i < randomWord.lenght; i++);
-        while (element != i);
+    for (let i = 0; i < randomWord.lenght; i++) {
+        if (i == element) {
+
+        } else {
+            lives -= 1;
+        };
+    };
 }
