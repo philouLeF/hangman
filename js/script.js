9789d569f2ca3a84fdcb12a2b65dcd6defe67316
philouLeF <delcroixphilippe73@gmail.com>
2022-03-10 09:49:46 +0100

    premières fondations

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const words = ["Javascript", "HTML", "CSS"];
+
+function init() {
+    let live = 5;
+    let randomWord = words[Math.floor(Math.random * words.length)]
+}
+
+function guess(element) {
+    for (let i = 0; i < randomWord.lenght; i++);
+        while (element != i);
+}
